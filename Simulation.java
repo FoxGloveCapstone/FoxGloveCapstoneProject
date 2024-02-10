a6250325b5848aa6e22998df73935e5edf473b91
KrimsonTiger <158619325+KrimsonTiger@users.noreply.github.com>
2024-02-10 15:34:28 -0500

    Add files via upload

---
+++ Simulation.java
@@ -0,0 +1,196 @@
+package simulation;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+/*
+ * Controls the flow and execution of the simulation. This will most likely be the central class. 
+ * Controls the timing and the main GUI. 
+ */
+public class Simulation extends JFrame {
+	private CellManager[] threads;
+	private int currentStep;
+
+	public static void main(String[] args) {
+        Simulation window = new Simulation();
+		// TODO: Create main window GUI
+        window.setVisible(true); // Open the window, making it visible on the screen.
+	}
+
+	public Simulation() {
+		super("Game of Life");
+
+		//Sets the panels being used
+		JPanel buttonBoard = new JPanel(new FlowLayout());
+		JPanel gridBoard = new JPanel(new GridLayout(10, 10));		
+		JPanel rules = new JPanel();		
+		JSplitPane ruleBoard = new JSplitPane(SwingConstants.HORIZONTAL, rules, buttonBoard);
+		//GridLayout gridLayout = new GridLayout();
+		
+		//gridBoard.add(gridLayout);
+		
+		//Sets Dimensions on side grid and divider location on the splitpane
+		ruleBoard.setDividerLocation(350);
+		gridBoard.setPreferredSize(new Dimension(800,900));
+		
+		//Buttons and Textfields for operations
+		JTextField rowsField = new JTextField("10");
+		JTextField columnsField = new JTextField("10");
+		rowsField.setColumns(10);
+		columnsField.setColumns(10);
+		JButton mapGenerateButton = new JButton("Generate Map");
+		JButton playButton = new JButton("Start");
+		JButton pauseButton = new JButton("Pause");
+		JButton nextButton = new JButton("Next");
+		JButton clearButton = new JButton("Clear");
+		
+		
+		mapGenerateButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+					String generateRows = rowsField.getText();
+					String generateColumns = columnsField.getText();
+					try {
+						int generateR = Integer.parseInt(generateRows);
+						int generateC = Integer.parseInt(generateColumns);
+						setVisible(false);
+						Grid grid = new Grid();
+						gridBoard.removeAll();
+						grid.generate(gridBoard, generateR, generateC);
+						setVisible(true);
+					}
+					catch(NumberFormatException s){
+				           JOptionPane.showMessageDialog(null, "Invalid Input! Please Enter a Number!", 
+                                   "ERROR", JOptionPane.ERROR_MESSAGE);
+					}
+			}
+		});
+		
+		playButton.addActionListener(new Play());
+		pauseButton.addActionListener(new Pause());
+		nextButton.addActionListener(new Step());
+		
+		clearButton.addActionListener(new Reset() {
+			public void actionPerformed(ActionEvent c) {
+				setVisible(false);
+				gridBoard.removeAll();
+				setVisible(true);
+			}
+		});
+		
+		//Labels for the rules section
+		JLabel rowsLabel = new JLabel ("Rows");
+		JLabel columnsLabel = new JLabel("Columns");
+		JLabel rules1 = new JLabel("--Any live cell with fewer than two live neighbours dies (referred to as underpopulation).");
+		JLabel rules2 = new JLabel("--Any live cell with more than three live neighbours dies (referred to as overpopulation).");
+		JLabel rules3 = new JLabel("--Any live cell with two or three live neighbours lives, unchanged, to the next generation.");
+		JLabel rules4 = new JLabel("--Any dead cell with exactly three live neighbours comes to life.");
+		JLabel rules5 = new JLabel("Rules for the Game Of Life");
+		JLabel rules6 = new JLabel("*************************************************************************************************");
+		JLabel fCounter = new JLabel("Frame Counter: ");
+
+		//Adds rules to ruleboard
+		rules.add(rules5);
+		rules.add(rules6);
+		rules.add(rules1);
+		rules.add(rules2);
+		rules.add(rules3);
+		rules.add(rules4);
+		
+		
+		//adds Buttons to ButtonBoard
+		buttonBoard.add(rowsLabel);
+		buttonBoard.add(rowsField);
+		buttonBoard.add(columnsLabel);
+		buttonBoard.add(columnsField);
+		buttonBoard.add(mapGenerateButton);
+		buttonBoard.add(playButton);
+		buttonBoard.add(pauseButton);
+		buttonBoard.add(nextButton);
+		buttonBoard.add(clearButton);
+		buttonBoard.add(fCounter);
+		
+		//Adds borderlayout and boards to the JFrame
+		add(gridBoard, BorderLayout.WEST);
+		add(ruleBoard, BorderLayout.CENTER);
+		
+	
+		//Close and Size functions for JFrame
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(new Dimension(1400, 800));
+	}
+	
+	/* Simulation Controls */
+	
+	public void start() {
+		
+	}
+	public void pause() {
+		
+	}
+	public void step() {
+		
+	}
+	public void reset() {
+		
+	}
+
+	
+	/*public static class Mapgenerate implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			
+			System.out.println("Mapgenerate");
+        }
+	}*/
+	
+	public static class Play implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.out.println("Play");
+        }
+	}
+	
+	public static class Pause implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.out.println("Pause");
+        }
+	}
+	
+	public static class Step implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.out.println("Step");
+        }
+	}
+	
+	public static class Reset implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.out.println("Reset");
+        }
+	}
+	
+}
